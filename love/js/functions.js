9ea07c9274cd1220537d31467ff785bbcb3418f7
724066266@qq.com <724066266@qq.com>
2016-04-12 12:20:19 +0800

    change mistake

--- love/js/functions.js
+++ love/js/functions.js
@@ -93,7 +93,7 @@ function startHeartAnimation() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
+	var current = new Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
